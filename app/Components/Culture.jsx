afb87d95049d4f66748f4447a50ed8e50c7c8338
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-08 01:21:28 +0500

    asd

---
+++ app/Components/Culture.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { font } from './font/font'
+
+const Culture = () => {
+  return (
+    <div className={`${font.className} bg-white p-12`}>
+        <h1 className='text-4xl font-bold tracking-wider'>Our Culture</h1>
+    </div>
+  )
+}
+
+export default Culture

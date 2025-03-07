4063f289c219b306bdbbd632c05d6c73d5d47f8b
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-08 00:21:16 +0500

    asd

---
+++ app/Components/Rating.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Rating = () => {
+  return (
+    <div className='bg-amber-50 py-12 w-full space-x-4 flex justify-center items-center'>
+        <div>
+            <img src='/rating1.webp'/>
+        </div>
+        
+        <div>
+            <img src='/rating2.webp'/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Rating
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from './Components/Hero';
 import Navbar from './Components/Navbar';
+import Rating from './Components/Rating';
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Rating/>
     </div>
   );
 }

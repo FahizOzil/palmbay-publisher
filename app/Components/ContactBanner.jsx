4b4a11900d62dc2325e4c0a72c496e825478af55
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-10 22:38:51 +0500

    Update ContactBanner.jsx

--- app/Components/ContactBanner.jsx
+++ app/Components/ContactBanner.jsx
@@ -3,12 +3,21 @@ import { font } from './font/font'
 
 const ContactBanner = () => {
   return (
-    <div className={`${font.className} flex flex-col mt-24 text-white justify-center items-center customgreen p-8`}>
-            <h1 className='text-7xl max-w-4xl text-center pt-12'>Contact Us Today to Start The Process</h1>
+    <div className='customgreen mt-24'>
+        <div>
+        <img src='/ring.png' className='w-32'/>
+
+        </div>
+        <div className={`${font.className} flex flex-col text-white justify-center items-center customgreen`}>
+            <h1 className='text-7xl max-w-4xl text-center'>Contact Us Today to Start The Process</h1>
             <button className="button-gradient mt-8 cursor-pointer px-8 py-3 rounded-md font-bold text-black hover:bg-yellow-400">
-              Get Started!
+                Get Started!
             </button>
         </div>
+        <div className=' flex justify-end items-center'>
+            <img src='/star.png' className='w-32'/>
+        </div>
+    </div>
   )
 }
 

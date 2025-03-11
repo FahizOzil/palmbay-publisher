140e02fbe202756cc39dd67f54cac20c72f545e0
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 00:59:14 +0500

    Update AudioDesign.jsx

--- app/services/audiobook-publishing/AudioDesign.jsx
+++ app/services/audiobook-publishing/AudioDesign.jsx
@@ -6,8 +6,8 @@ const AudioDesign = () => {
     <div className={`${font.className} flex flex-col mt-20 justify-center items-center text-black px-4 text-center`}>
           <p className="text-3xl md:text-4xl">Silver Gate Publishing Audiobook Service</p>
           <p className="pt-4 text-md md:text-xl max-w-3xl">
-          Check out our services below to find the right fit for your needs!</p>
-          <p className="pt-8 text-xl italic max-w-3xl">*Our cover design services are automatically formatted for paperback books. Additional fees are required for converting to hardcover and/or e-book.</p>   
+          Discover the transformative power of turning your written words into captivating audio experiences that connect with a broader audience.</p>
+          {/* <p className="pt-8 text-xl italic max-w-3xl">*Our cover design services are automatically formatted for paperback books. Additional fees are required for converting to hardcover and/or e-book.</p>    */}
           <div className='flex flex-col md:space-x-8 md:flex-row mt-16 justify-center items-center'>
             <img data-aos="fade-right" src='/cover1.webp' className='w-96 h-96' />
             <div className='flex flex-col justify-start items-start p-2'>

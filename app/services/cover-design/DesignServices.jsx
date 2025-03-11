dcbb440a694cff6d872bffa48bc4521d5ce270df
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-11 22:26:35 +0500

    Update DesignServices.jsx

--- app/services/cover-design/DesignServices.jsx
+++ app/services/cover-design/DesignServices.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 const DesignServices = () => {
   return (
     <div className={`${font.className
-    } flex flex-col mt-12 justify-center items-center text-black px-4 text-center`}>
-          <p className="text-3xl md:text-6xl">Book Cover Design Services</p>
+    } flex flex-col mt-20 justify-center items-center text-black px-4 text-center`}>
+          <p className="text-3xl md:text-4xl">Book Cover Design Services</p>
           <p className="pt-4 text-md md:text-xl max-w-3xl">
           Check out our services below to find the right fit for your needs!</p>
           <p className="pt-8 text-xl italic max-w-3xl">*Our cover design services are automatically formatted for paperback books. Additional fees are required for converting to hardcover and/or e-book.</p>   
-        </div>
+    </div>
   )
 }
 

d16790c76722033f34f523a81fe2a4cc9221a942
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 00:52:04 +0500

    Create AudioFormatting.jsx

---
+++ app/services/audio-book/AudioFormatting.jsx
@@ -0,0 +1,22 @@
+import { font } from '@/app/Components/font/font'
+import React from 'react'
+
+const AudioFormatting = () => {
+  return (
+    <div className={`${font.className} mt-20`}>
+        <div className="relative  w-full min-h-[25rem] bg-cover bg-center" style={{ backgroundImage: "url('/whitewood.png')" }}>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-black px-4 text-center">
+          <p className="text-3xl md:text-6xl">Audio Book Formatting</p>
+          <p className="pt-4 text-md md:text-xl max-w-3xl">
+          Interior Formatting goes hand in hand with our Cover Design Services. This service ensures your manuscript is professionally formatted and optimized for publishing. Utilizing both services gives you a seamless publishing experience, letting us handle all the technical aspects of layout, typography, and design.
+          </p>
+          <button className="mt-8 button-gradient cursor-pointer px-8 py-3 rounded-md font-bold text-black hover:bg-yellow-400">
+            Learn More!
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AudioFormatting

e8b91dde214583a5b0bde583cfd0cec025c8b835
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 01:26:51 +0500

    Update AudioFormatting.jsx

--- app/services/audiobook-publishing/AudioFormatting.jsx
+++ app/services/audiobook-publishing/AudioFormatting.jsx
@@ -6,9 +6,9 @@ const AudioFormatting = () => {
     <div className={`${font.className} mt-20`}>
         <div className="relative  w-full min-h-[25rem] bg-cover bg-center" style={{ backgroundImage: "url('/whitewood.png')" }}>
         <div className="absolute inset-0 flex flex-col justify-center items-center text-black px-4 text-center">
-          <p className="text-3xl md:text-6xl">Audio Book Formatting</p>
+          <p className="text-3xl md:text-6xl">Superior Quality Audiobook Creation</p>
           <p className="pt-4 text-md md:text-xl max-w-3xl">
-          Interior Formatting goes hand in hand with our Cover Design Services. This service ensures your manuscript is professionally formatted and optimized for publishing. Utilizing both services gives you a seamless publishing experience, letting us handle all the technical aspects of layout, typography, and design.
+          Our service helps you publish a high-quality audiobook without breaking the bank. You can also set your narrator preferences based on gender, accent, and tone of voice. We pair you with the perfect narrator to bring your book to life. Once complete, Silver Gate helps you sell your audiobook through Audible and Apple.
           </p>
           <button className="mt-8 button-gradient cursor-pointer px-8 py-3 rounded-md font-bold text-black hover:bg-yellow-400">
             Learn More!

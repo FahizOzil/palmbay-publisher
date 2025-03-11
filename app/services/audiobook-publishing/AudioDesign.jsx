f64d869a76294708fb826fe8d9b148c9a586bbb6
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 00:58:48 +0500

    Update AudioDesign.jsx

--- app/services/audiobook-publishing/AudioDesign.jsx
+++ app/services/audiobook-publishing/AudioDesign.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AudioDesign = () => {
   return (
     <div className={`${font.className} flex flex-col mt-20 justify-center items-center text-black px-4 text-center`}>
-          <p className="text-3xl md:text-4xl">Book Cover Design Services</p>
+          <p className="text-3xl md:text-4xl">Silver Gate Publishing Audiobook Service</p>
           <p className="pt-4 text-md md:text-xl max-w-3xl">
           Check out our services below to find the right fit for your needs!</p>
           <p className="pt-8 text-xl italic max-w-3xl">*Our cover design services are automatically formatted for paperback books. Additional fees are required for converting to hardcover and/or e-book.</p>   

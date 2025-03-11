6eafd22760ebc1abbf1b1c23cad279143423f557
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 00:03:30 +0500

    Update InteriorFormatting.jsx

--- app/services/cover-design/InteriorFormatting.jsx
+++ app/services/cover-design/InteriorFormatting.jsx
@@ -12,7 +12,7 @@ const InteriorFormatting = () => {
           </p>
           <p className="pt-8 text-xl italic max-w-3xl">*Cover Design and Interior Formatting are required services.</p>   
           <button className="mt-8 button-gradient cursor-pointer px-8 py-3 rounded-md font-bold text-black hover:bg-yellow-400">
-            Get Started!
+            !
           </button>
         </div>
       </div>

3a99e376fbec9c945adca26733e8b2bb657a649b
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-11 21:56:28 +0500

    Update page.jsx

--- app/services/cover-design/page.jsx
+++ app/services/cover-design/page.jsx
@@ -6,7 +6,8 @@ const page = () => {
   return (
     <div className={`${font.className}`}>
       <Navbar/>
-      <div className='flex flex-col pt-20 justify-center items-center'>
+      <div className="relative w-full min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/whitewood.png')" }}></div>
+      <div className='flex relative flex-col pt-20 justify-center items-center'>
           <p className='text-6xl'>Book Cover Design</p>
           <p className='pt-4 text-xl max-w-3xl text-center'>Silver Gate Publishing offers four different professional book cover design services tailored to meet the needs of any independent author based on their budget and design preferences. Learn more about our services below and how we can help you design a captivating book cover.</p>
           <p className='pt-8 text-xl italic max-w-3xl text-center'>*Cover Design and Interior Formatting are required services.</p>

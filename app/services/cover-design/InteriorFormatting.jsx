a3a0b9063f073eaa0f9d4902648a173e748d7b3a
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 00:02:12 +0500

    Update InteriorFormatting.jsx

--- app/services/cover-design/InteriorFormatting.jsx
+++ app/services/cover-design/InteriorFormatting.jsx
@@ -4,7 +4,18 @@ import React from 'react'
 const InteriorFormatting = () => {
   return (
     <div className={`${font.className}`}>
-
+        <div className="relative  w-full min-h-[30rem] bg-cover bg-center" style={{ backgroundImage: "url('/whitewood.png')" }}>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-black px-4 text-center">
+          <p className="text-3xl md:text-6xl">Interior Formatting</p>
+          <p className="pt-4 text-md md:text-xl max-w-3xl">
+            Silver Gate Publishing offers four different professional book cover design services tailored to meet the needs of any independent author based on their budget and design preferences. Learn more about our services below and how we can help you design a captivating book cover.
+          </p>
+          <p className="pt-8 text-xl italic max-w-3xl">*Cover Design and Interior Formatting are required services.</p>   
+          <button className="mt-8 button-gradient cursor-pointer px-8 py-3 rounded-md font-bold text-black hover:bg-yellow-400">
+            Get Started!
+          </button>
+        </div>
+      </div>
     </div>
   )
 }

de2d5d503cfb85437d8ccfbeb0ec25ccdccc7fe6
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-11 23:23:36 +0500

    Update DesignServices.jsx

--- app/services/cover-design/DesignServices.jsx
+++ app/services/cover-design/DesignServices.jsx
@@ -38,6 +38,21 @@ const DesignServices = () => {
             </div>
             <img data-aos="fade-right" src='/cover2.webp' className='w-96 h-96' />
         </div>
+          <div className='flex flex-col md:space-x-8 md:flex-row mt-16 justify-center items-center'>
+            <img data-aos="fade-right" src='/cover3.webp' className='w-96 h-96' />
+            <div className='flex flex-col justify-start items-start p-2'>
+                <h1 data-aos="fade-left" className='text-3xl md:text-5xl mt-6 md:max-w-xl text-left'>Complex Cover</h1>
+                <p data-aos="fade-up" className='green-text text-3xl md:max-w-xl mt-4 text-left'>
+                    Starting at $899
+                </p>
+                <div className='space-y-2 mt-4 text-xl text-left'>
+                <p data-aos="fade-up-right"><span className='green-text'>&#x2022;</span>&nbsp;2 Cover concepts with front, spine, and back design</p>
+                <p data-aos="fade-up-left"><span className='green-text'>&#x2022;</span>&nbsp;Up to 2 fully licensed premium stock images per concept</p>
+                <p data-aos="fade-up-right"><span className='green-text'>&#x2022;</span>&nbsp;2 Round of changes</p>
+                <p data-aos="fade-up-left"><span className='green-text'>&#x2022;</span>&nbsp;Appropriate for all genres</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }

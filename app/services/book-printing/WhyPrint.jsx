0377f5376a31c1d65601d074f642d5fbfe3a6cbd
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 23:55:57 +0500

    Update WhyPrint.jsx

--- app/services/book-printing/WhyPrint.jsx
+++ app/services/book-printing/WhyPrint.jsx
@@ -6,7 +6,7 @@ const WhyPrint = () => {
     <div className={`${font.className} mt-20`}>
         <div className="relative  w-full min-h-[25rem] bg-cover bg-center" style={{ backgroundImage: "url('/whitewood.png')" }}>
         <div className="absolute inset-0 flex flex-col justify-center items-center text-black px-4 text-center">
-          <p className="text-3xl md:text-6xl">Why</p>
+          <p className="text-3xl md:text-6xl">Why </p>
           <p className="pt-4 text-md md:text-xl max-w-3xl">
           Our service helps you publish a high-quality audiobook without breaking the bank. You can also set your narrator preferences based on gender, accent, and tone of voice. We pair you with the perfect narrator to bring your book to life. Once complete, Silver Gate helps you sell your audiobook through Audible and Apple.
           </p>

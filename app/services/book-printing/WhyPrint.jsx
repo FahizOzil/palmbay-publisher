5079ebd2f14b2843abe8da9a9341a3ac8bafbd23
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-13 00:00:29 +0500

    Update WhyPrint.jsx

--- app/services/book-printing/WhyPrint.jsx
+++ app/services/book-printing/WhyPrint.jsx
@@ -8,7 +8,7 @@ const WhyPrint = () => {
         <div className="absolute inset-0 flex flex-col justify-center items-center text-black px-4 text-center">
           <p className="text-3xl md:text-6xl">Why Print With Us</p>
           <p className="pt-4 text-md md:text-xl max-w-3xl">
-          Our service helps you publish a high-quality audiobook without breaking the bank. You can also set your narrator preferences based on gender, accent, and tone of voice. We pair you with the perfect narrator to bring your book to life. Once complete, Silver Gate helps you sell your audiobook through Audibl
+          Our service helps you publish a high-quality audiobook without breaking the bank. You can also set your narrator preferences based on gender, accent, and tone of voice. We pair you with the perfect narrator to bring your book to life. Once complete, Silver Gate helps you sell your audiobook through Audib
           </p>
           <button className="mt-8 button-gradient cursor-pointer px-8 py-3 rounded-md font-bold text-black hover:bg-yellow-400">
             Learn More!

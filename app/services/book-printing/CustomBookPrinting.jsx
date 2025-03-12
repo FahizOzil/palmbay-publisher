88434b06887597cfb52449fed63678e0fc82f291
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-12 22:04:20 +0500

    Update CustomBookPrinting.jsx

--- app/services/book-printing/CustomBookPrinting.jsx
+++ app/services/book-printing/CustomBookPrinting.jsx
@@ -49,18 +49,18 @@ const CustomBookPrinting = () => {
             <div className='flex flex-col justify-start items-start p-2'>
                 <h1 data-aos="fade-left" className='text-3xl green-text md:text-5xl mt-6 md:max-w-xl text-left'>Full Color Printing</h1>
                 <div className='space-y-2 mt-4 text-xl text-left'>
-                <p className='max-w-2xl' data-aos="fade-up-right"><span className='green-text'>&#x2022;</span>&nbsp;The interior of your book should be just as captivating as the cover. Palmetto’s vibrant, full-color image and custom interior text printing options ensure that every page of your book is a canvas of cohesion. We help your narrative unfold seamlessly, connecting words and imagery with artistic precision.</p>
+                <p className='max-w-2xl' data-aos="fade-up-right"><span className='green-text'>&#x2022;</span>&nbsp;The interior of your book should be just as captivating as the cover. Silver Gate's vibrant, full-color image and custom interior text printing options ensure that every page of your book is a canvas of cohesion. We help your narrative unfold seamlessly, connecting words and imagery with artistic precision.</p>
                 <button className="button-gradient mt-6 md:mt-8 px-6 sm:px-8 py-3 rounded-md font-bold text-black transition duration-300 hover:bg-yellow-400">
           Get Started!
         </button>
                 </div>
             </div>
-            <img data-aos="fade-right" src='/print4.webp' className='w-96 h-96' />
+            <img data-aos="fade-right" src='/print5.webp' className='w-96 h-96' />
         </div>
         <div className='flex flex-col md:space-x-8 md:flex-row mt-16 justify-center items-center'>
             <img data-aos="fade-right" src='/print3.webp' className='w-96 h-96' />
             <div className='flex flex-col justify-start items-start p-2'>
-                <h1 data-aos="fade-left" className='text-3xl green-text md:text-5xl mt-6 md:max-w-xl text-left'>Custom Book Cover Finishes</h1>
+                <h1 data-aos="fade-left" className='text-3xl green-text md:text-5xl mt-6 md:max-w-xl text-left'>Interior Paper Stock Options</h1>
                 <div className='space-y-2 mt-4 text-xl text-left'>
                 <p className='max-w-2xl' data-aos="fade-up-right"><span className='green-text'>&#x2022;</span>&nbsp;A professionally designed book cover is an excellent attention grabber, and yours deserves to be unforgettable. Palmetto Publishing is here to turn your work into a total sensory experience. Enhance your book cover’s allure by using a soft matte or polished gloss finish for an added touch of visual and tactile refinement.</p>
                

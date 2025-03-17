4b9d51730c7dd3641e69231c91db63fc80ab2717
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-18 02:00:35 +0500

    Update page.jsx

--- app/services/book-formatting/page.jsx
+++ app/services/book-formatting/page.jsx
@@ -16,17 +16,17 @@ const page = () => {
       <Navbar />
       <div className="relative  w-full min-h-[40rem] bg-cover bg-center" style={{ backgroundImage: "url('/whitewood.png')" }}>
         <div className="absolute inset-0 flex flex-col justify-center items-center text-black px-4 text-center">
-          <p className="text-3xl md:text-6xl">Book Printing Services</p>
+          <p className="text-3xl md:text-6xl">Book Formatting Services</p>
           <p className="pt-4 text-md md:text-xl max-w-3xl">
-          Ask any published author and they’ll tell you that few life experiences can beat the excitement of holding your book in your hands for the first time. After all of the work that goes into writing, editing, and designing, it feels like Christmas morning unboxing the first shipment of books hot off the press.
+          Elevate your writing to the next level with a professionally designed and formatted book interior.
           </p>
-          <p className="pt-8 text-xl max-w-3xl">When you’re ready to transform your words into a tangible work of art, Silver Gate Publishing is your trusted partner for premium book printing services. We take pride in printing the highest-quality books and provide a seamless and exceptional printing experience for independent authors, bringing your words to life on the canvas of a professionally printed book.</p>   
+          <p className="pt-6 text-xl max-w-3xl">Silver Gate Publishing draws from decades of collective experience to help you develop the layout of your interior pages so they perfectly speak to your readers. Learn more about how we can unlock your manuscript’s potential, transforming it into a polished masterpiece.</p>   
           <button className="mt-8 button-gradient cursor-pointer px-8 py-3 rounded-md font-bold text-black hover:bg-yellow-400">
             Get Started!
           </button>
         </div>
       </div>
-      <Solutions word={"Printing"}/>
+      <Solutions word={"Formatting"}/>
       {/* <CustomBookPrinting/>
       <HowWePrepare/>
       <Binding/>

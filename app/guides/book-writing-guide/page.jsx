77ec4e6bc7287aac9760e00c1450256b7509008f
Anonymous <111380595+haseebx16@users.noreply.github.com>
2025-03-20 01:40:03 +0500

    Create page.jsx

---
+++ app/guides/book-writing-guide/page.jsx
@@ -0,0 +1,46 @@
+import { font } from '@/app/Components/font/font'
+import Navbar from '@/app/Components/Navbar'
+import React from 'react'
+import MarketingSection from '../marketing-tips/MarketingSection'
+
+const page = () => {
+  return (
+    <div>
+    <div className={`${font.className} relative`}>
+      <Navbar />
+      <div className="relative w-full min-h-[30rem] bg-cover bg-center" style={{ backgroundImage: "url('/whitewood.png')" }}>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-black px-4 text-center">
+          <p className="text-3xl mb-12 md:text-6xl">
+          The Complete Guide to: <br /> Writing A Book
+          </p>
+        </div>
+      </div>
+
+      {/* Left-side image */}
+      <img 
+        src="/weird-moon.webp" 
+        className="rounded-md h-64 absolute bottom-[-200px] left-[120px] z-10" 
+      />
+
+      {/* Centered Image */}
+      <div className="relative w-full flex justify-center">
+        <img 
+          src="/book-marketing.webp" 
+          className="rounded-md h-96 absolute bottom-[-200px] z-20" 
+        />
+      </div>
+
+      {/* Right-side image FIXED */}
+      <div className="w-full relative">
+        <img 
+          src="/weird.png" 
+          className="rounded-md h-64 absolute bottom-[-200px] right-[120px] z-10" 
+        />
+      </div>
+    </div>
+      <MarketingSection/>
+    </div>
+  )
+}
+
+export default page;
